157fb912ff1f415da9568ff3ec5bad7bc89b4e2a
Matthew Brett <matthew.brett@gmail.com>
2023-04-03 13:11:08 +0100

    Relithink missing values

--- coincidence.py
+++ coincidence.py
@@ -18,10 +18,9 @@ def link_table(series_ids):
     series_ids = np.array(series_ids, dtype=float)
     n = len(series_ids)
     arr = np.zeros((n, n))
-    for i, series in enumerate(series_ids):
-        if np.isnan(series):
-            continue
-        arr[:, i] = series_ids == series
+    col, row = series_ids[:, None], series_ids[None, :]
+    arr[:, :] = col == row
+    arr[np.isnan(col + row)] = np.nan
     return arr
 
 
@@ -33,8 +32,12 @@ def jaccard_table(col):
 
     See :func:`jaccard_table_slow` for a version that might be easier to read.
     """
-    col = np.array(col, dtype=float)
-    return np.outer(col, col)
+    col = np.array(col, dtype=float)[:, None]  # Row
+    res = col * col.T
+    # If either value is 0, 0 overrides nan
+    is_zero = col == 0
+    res[is_zero | is_zero.T] = 0
+    return res
 
 
 def jaccard_table_slow(col):
@@ -52,30 +55,50 @@ def funcand(a, b):
     return a and b
 
 
-def sameas(col, op=funcand):
-    # Another slow calculation for illustration.
+def equal_to(col):
+    # Slow calculation for illustration.
     col = np.array(col, dtype=float)
     n = len(col)
     arr = np.zeros((n, n))
     for i in range(n):
         for j in range(n):
-            if op(col[i], col[j]):
+            a, b = col[i], col[j]
+            if np.isnan(a) or np.isnan(b):
+                arr[i, j] = np.nan
+            else:
+                arr[i, j] = a == b
+    return arr
+
+
+def both_1(col):
+    # Slow calculation for illustration.
+    col = np.array(col, dtype=float)
+    n = len(col)
+    arr = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            a, b = col[i], col[j]
+            if 0 in (a, b):
+                arr[i, j] = 0
+            elif np.isnan(a + b):
+                arr[i, j] = np.nan
+            else:
                 arr[i, j] = 1
     return arr
 
 
 def calc_ratios(link_pairs, jacc_pairs):
+    not_nan = ~np.isnan(link_pairs + jacc_pairs)
+    link_pairs, jacc_pairs = link_pairs[not_nan], jacc_pairs[not_nan]
     n_links = np.sum(link_pairs)
     n_jaccs = np.sum(jacc_pairs)
-    n_pairs = len(link_pairs)
     # Multiply the links and Jaccards, and sum them to get the number
     # of links that are also Jaccards.
-    # Equivalent to np.sum(link_pairs * jacc_pairs)
-    n_jl = np.dot(link_pairs, jacc_pairs)
+    n_jl = np.sum(link_pairs * jacc_pairs)
     # Linked Jaccards divided by all links.
     jl_rat = n_jl / n_links
     # Non-linked Jaccards divided by all non-links.
-    jnl_rat = (n_jaccs - n_jl) / (n_pairs - n_links)
+    jnl_rat = (n_jaccs - n_jl) / (len(link_pairs) - n_links)
     return jl_rat, jnl_rat
 
 
@@ -85,7 +108,7 @@ def test_jaccard():
     # Presence of feature
     feature = [0, 0, 1, 0, 1, 1, 1, 0]
     link_arr = link_table(in_col)
-    assert np.all(link_arr == sameas(in_col, operator.eq))
+    assert np.all(link_arr == equal_to(in_col))
     # Extract lower triangle, and unravel to 1D.
     inds = np.tril_indices(8, -1)
     link_pairs = link_arr[inds]
@@ -108,10 +131,14 @@ def test_jaccard():
                    0, 0, 0, 0, 0, 0, 0])
     # Check slow version gives same answer.
     assert np.all(jacc_pairs == jaccard_table_slow(feature)[inds])
-    assert np.all(jacc_pairs == sameas(feature)[inds])
+    assert np.all(jacc_pairs == both_1(feature)[inds])
     jl_r, jnl_r = calc_ratios(link_pairs, jacc_pairs)
     assert np.isclose(jl_r, 3 / 4)
     assert np.isclose(jnl_r, 3 / 24)
+
+
+def nan_eq(a, b):
+    return np.array_equal(a, b, equal_nan=True)
 
 
 def test_jaccard_nan():
@@ -120,33 +147,33 @@ def test_jaccard_nan():
     # Presence of feature
     feature = [0, np.nan, 1, 0, 1, np.nan, 1, 0]
     link_arr = link_table(in_col)
-    assert np.all(link_arr == sameas(in_col, operator.eq))
+    assert nan_eq(link_arr, equal_to(in_col))
     # Extract lower triangle, and unravel to 1D.
     inds = np.tril_indices(8, -1)
     link_pairs = link_arr[inds]
-    assert np.all(link_pairs ==
+    assert nan_eq(link_pairs,
                   [0,
                    0, 1,
                    0, 0, 0,
                    0, 0, 0, 0,
-                   0, 0, 0, 0, 1,
-                   0, 0, 0, 0, 1, 1,
-                   0, 0, 0, 0, 0, 0, 0])
+                   np.nan, np.nan, np.nan, np.nan, np.nan,
+                   0, 0, 0, 0, 1, np.nan,
+                   0, 0, 0, 0, 0, np.nan, 0])
     jacc_pairs = jaccard_table(feature)[inds]
-    assert np.all(jacc_pairs ==
-                  [0,
-                   0, 0,
-                   0, 0, 0,
-                   0, 0, 1, 0,
-                   0, 0, 1, 0, 1,
-                   0, 0, 1, 0, 1, 1,
-                   0, 0, 0, 0, 0, 0, 0])
     # Check slow version gives same answer.
-    assert np.all(jacc_pairs == jaccard_table_slow(feature)[inds])
-    assert np.all(jacc_pairs == sameas(feature)[inds])
+    assert nan_eq(jacc_pairs, both_1(feature)[inds])
+    assert nan_eq(jacc_pairs,
+                  [0,
+                   0, np.nan,
+                   0, 0, 0,
+                   0, np.nan, 1, 0,
+                   0, np.nan, np.nan, 0, np.nan,
+                   0, np.nan, 1, 0, 1, np.nan,
+                   0, 0, 0, 0, 0, 0, 0])
     jl_r, jnl_r = calc_ratios(link_pairs, jacc_pairs)
-    assert np.isclose(jl_r, 3 / 4)
-    assert np.isclose(jnl_r, 3 / 24)
+    assert np.isclose(jl_r, 1)
+    assert np.isclose(jnl_r, 2 / 17)
+
 
 def test_jaccard2():
     # Case links (case IDs)
@@ -154,7 +181,7 @@ def test_jaccard2():
     # Presence of feature
     feature = [1, 1, 0, 0, 1]
     link_arr = link_table(in_col)
-    assert np.all(link_arr == sameas(in_col, operator.eq))
+    assert np.all(link_arr == equal_to(in_col))
     # Extract lower triangle, and unravel to 1D.
     inds = np.tril_indices(5, -1)
     link_pairs = link_arr[inds]
@@ -171,7 +198,7 @@ def test_jaccard2():
                    1, 1, 0, 0])
     # Check slow version gives same answer.
     assert np.all(jacc_pairs == jaccard_table_slow(feature)[inds])
-    assert np.all(jacc_pairs == sameas(feature)[inds])
+    assert np.all(jacc_pairs == both_1(feature)[inds])
     jl_r, jnl_r = calc_ratios(link_pairs, jacc_pairs)
     assert np.isclose(jl_r, 1 / 3)
     assert np.isclose(jnl_r, 2 / 7)
